2729a78fb86a969df7b588e9fb821e8d0d445b2e
Mike Jones, II <micujonesii@gmail.com>
2025-01-24 08:13:58 -0500

    Set up form to handle submissions

--- components/login-view/login-view.jsx
+++ components/login-view/login-view.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const LoginView = () => {
-    
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+
     return (
         <form action="">
             <label>Username
-                <input type="text" />
+                <input type="text"
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} 
+                required />
             </label>
             <label>Password
-                <input type="password" />
+                <input type="password" 
+                value={password} 
+                onChange={(e) => setUsername(e.target.value)} 
+                required/>
             </label>
             <button type="submit">SUBMIT</button>
         </form>
