6e7526d8cb3902227035916b46480fb35390b7e5
Mike Jones, II <micujonesii@gmail.com>
2025-01-24 08:08:52 -0500

    Create login form

---
+++ components/login-view/login-view.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const LoginView = () => {
+    
+    return (
+        <form action="">
+            <label>Username
+                <input type="text" />
+            </label>
+            <label>Password
+                <input type="password" />
+            </label>
+            <button type="submit">SUBMIT</button>
+        </form>
+    )
+}
