1339ca05257e55ba72f8186df70f2bdedec1ae9d
Mike Jones, II <micujonesii@gmail.com>
2025-01-29 09:42:03 -0500

    Import stylesheet

--- components/movie-view/movie-view.jsx
+++ components/movie-view/movie-view.jsx
@@ -1,17 +1,22 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import './movie-view.scss';
 
 export const MovieView = ({ movie, onBackClick }) => {
     return (
         <div>
             <img src={movie.image} height="425px" />
             <h1>{movie.title}</h1>
-            <p>Starring {movie.actors[0]} and {movie.actors[1]}</p>
+            <p>
+                Starring {movie.actors[0]} and {movie.actors[1]}
+            </p>
             <p>Directed by {movie.director.name}</p>
             <p>{movie.description}</p>
-            <button onClick={onBackClick}>Back</button>
+            <button className="back-button" onClick={onBackClick}>
+                Back
+            </button>
         </div>
     );
-}
+};
 
 MovieView.propType = {
     movie: PropTypes.shape({
@@ -21,7 +26,7 @@ MovieView.propType = {
         actors: PropTypes.array.isRequired,
         genre: PropTypes.object,
         image: PropTypes.string,
-        featured: PropTypes.bool
+        featured: PropTypes.bool,
     }).isRequired,
-    onBackClick: PropTypes.func.isRequired
-}
+    onBackClick: PropTypes.func.isRequired,
+};
