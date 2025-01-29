bd7956e78bf7479e68fc19c13c54a6d38ee81368
Mike Jones, II <micujonesii@gmail.com>
2025-01-29 08:39:29 -0500

    Update submit form fields

--- components/signup-view/signup-view.jsx
+++ components/signup-view/signup-view.jsx
@@ -1,19 +1,50 @@
+import { useState } from 'react';
+
 export const SignupView = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthday, setBirthday] = useState('');
+
     const handleSubmit = (event) => {};
 
     return (
         <form onSubmit={handleSubmit}>
             <label>
                 Email
-                <input type="email" />
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
             </label>
             <label>
                 Username
-                <input type="text" />
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
             </label>
             <label>
                 Password
-                <input type="password" />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </label>
+            <label>
+                Birthday
+                <input
+                    type="date"
+                    value={birthday}
+                    onChange={(e) => setBirthday(e.target.value)}
+                    required
+                />
             </label>
             <button type="submit">SUBMIT</button>
         </form>
