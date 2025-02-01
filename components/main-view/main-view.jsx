0989fffb46fdf637840f5e35acbd5a9f477cfa2a
Mike Jones, II <micujonesii@gmail.com>
2025-01-31 22:47:14 -0500

    Add navbar to main view

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -5,8 +5,8 @@ import { LoginView } from '../login-view/login-view';
 import { SignupView } from '../signup-view/signup-view';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { NavigationBar } from '../navigation-bar/navigation-bar';
 
 export const MainView = () => {
     // User data variables
@@ -46,6 +46,15 @@ export const MainView = () => {
 
     return (
         <BrowserRouter>
+            <NavigationBar
+                user={user}
+                onLoggedOut={(user) => {
+                    setUser(null);
+                    setToken(null);
+                    localStorage.clear();
+                }}
+            />
+            <Row></Row>
             <Row className="justify-content-md-center">
                 <Routes>
                     <Route
