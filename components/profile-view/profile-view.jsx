f323a9f7abbd3aa0dd86491325364b428bb49a0e
Mike Jones, II <micujonesii@gmail.com>
2025-02-06 15:45:33 -0500

    Remove console logs

--- components/profile-view/profile-view.jsx
+++ components/profile-view/profile-view.jsx
@@ -1,10 +1,5 @@
-// Add a user Profile view (linked to the main navigation menu for authenticated users). As per your project brief, this view should:
-// Display user information. You can obtain the logged in user information by using the /users endpoint and then filtering the returning list by comparing the usernames;
 // Allow a user to update their user information (username, password, email, date of birth);
 // Allow a user to deregister;
-// Display a user's favorite movies as a list. For this you’ll need to filter the movies array with something like let favoriteMovies = movies.filter(m => user.FavoriteMovies.includes(m._id)). With this, favoriteMovies is just a regular array of movie objects which could, for example, be displayed as movie cards (the MovieCard components would need to be passed to the ProfileView component and rendered there);
-// Add a “Favorite” button to your MovieCard and/or MovieView components, so that logged in users can select a movie to store in their list of favorites;
-// Allow a user to remove a movie from their list of favorites.
 
 import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
@@ -13,7 +8,7 @@ import { useParams } from 'react-router';
 import { MovieCard } from '../movie-card/movie-card';
 
 export const ProfileView = ({ movies }) => {
-    // Reference to user
+    // Reference to user in URL
     const { userId } = useParams();
 
     const [user, setUser] = useState({
@@ -43,8 +38,6 @@ export const ProfileView = ({ movies }) => {
 
     return (
         <>
-            {console.log('Opened profile url:', user)}
-            {console.log('Favorite movies:', favoriteMovies)}
             <div>
                 <h1>{user.username}</h1>
                 <p>{user.email}</p>
