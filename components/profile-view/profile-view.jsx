8f4447e62f7c34ebed0e9005d51a74545c6d46cc
Mike Jones, II <micujonesii@gmail.com>
2025-02-06 15:18:58 -0500

    Display favorite movies

--- components/profile-view/profile-view.jsx
+++ components/profile-view/profile-view.jsx
@@ -7,7 +7,10 @@
 // Allow a user to remove a movie from their list of favorites.
 
 import { useEffect, useState } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { useParams } from 'react-router';
+import { MovieCard } from '../movie-card/movie-card';
 
 export const ProfileView = ({ movies }) => {
     // Reference to user
@@ -31,16 +34,32 @@ export const ProfileView = ({ movies }) => {
     };
 
     useEffect(() => {
-        // getUser();
+        getUser();
     }, []);
+
+    const favoriteMovies = movies.filter((movie) =>
+        user.favoriteMovies.includes(movie.id)
+    );
 
     return (
         <>
-            {console.log(user)}
-            {/* <div>{user.username}</div>
+            {console.log('Opened profile url:', user)}
+            {console.log('Favorite movies:', favoriteMovies)}
             <div>
-                <h2>Favorite movies ({user.favoriteMovies.length}):</h2>
-            </div> */}
+                <h1>{user.username}</h1>
+                <p>{user.email}</p>
+            </div>
+            <br />
+            <div>
+                <h2>Favorite movies ({favoriteMovies.length}):</h2>
+                <Row>
+                    {favoriteMovies.map((movie) => (
+                        <Col className="mb-4" key={movie.id} md={3}>
+                            <MovieCard movie={movie} />
+                        </Col>
+                    ))}
+                </Row>
+            </div>
         </>
     );
 };
